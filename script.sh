7ea34c66c985ac68ae66b5d07d5603966138178e
Ahmed Sabry <108468895+Ahmed-Sabri@users.noreply.github.com>
2024-04-28 10:50:10 +0300

    Update script.sh
    Adding Rust, Brew, and Poetry

--- script.sh
+++ script.sh
@@ -5,6 +5,18 @@
 ######################################
 sudo apt update && sudo apt upgrade -y
 sudo apt install python3 python3-pip python3-full git curl glances gpg -y
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+curl -sSL https://install.python-poetry.org | python3 -
+export PATH="$HOME/.local/bin:$PATH"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.zshrc     
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+brew install gcc
+
 sudo apt install zsh -y
 sudo apt install zsh-syntax-highlighting zsh-autosuggestions -y
 sudo apt install qterminal fonts-firacode gnome-tweaks -y
@@ -22,16 +34,44 @@ zsh
 ######################################
 ## Phase-1.2 | Install NodeJS V21
 ######################################
-apt purge nodejs -y
-apt remove nodejs -y
-apt autoremove -y
-apt update && apt full-upgrade -y
-apt install -y ca-certificates curl gnupg
+sudo apt install nodejs -y
+sudo apt purge nodejs -y
+sudo apt remove nodejs -y
+sudo apt autoremove -y
+sudo apt update && sudo apt full-upgrade -y
+sudo apt install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_21.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-apt update && apt full-upgrade -y
-apt install nodejs -y
+sudo apt update && sudo apt full-upgrade -y
+sudo apt install nodejs -y
+pip install npm
+npm install pnpm
+######################################
+## Phase-1.3 | Install Docker
+######################################
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin -y
+
+######################################
+## Phase-1.4 | Install PIPX
+######################################
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+sudo pipx ensurepath
+
 ######################################
 ## Phase-2 | Install Miniconda 
 ######################################
